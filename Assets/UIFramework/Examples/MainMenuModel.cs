0a61f4de8e4c35bd8b4c7df7f8a4438c474421bc
hellsing <rainknightpox@gmail.com>
2025-03-03 16:08:20 +0800

    界面设计

--- Assets/UIFramework/Examples/MainMenuModel.cs
+++
@@ -1,8 +0,0 @@
-using UIFramework.Binding;
-
-public class MainMenuModel
-{
-    public BindableProperty<string> PlayerName { get; } = new BindableProperty<string>("Player");
-    public BindableProperty<int> Coins { get; } = new BindableProperty<int>(0);
-    public BindableProperty<bool> IsSoundEnabled { get; } = new BindableProperty<bool>(true);
-}
